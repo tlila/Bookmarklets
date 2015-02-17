bb927bca0144466220279a3059b282cebde8d7be
MITSUISHI, Tomomi <tomomi.mitsuishi@gmail.com>
2015-02-17 16:45:30 +0900

    Open database

--- IndexedDB.js
+++ IndexedDB.js
@@ -4,3 +4,18 @@ if (indexedDB) {
 } else {
   window.alert("IndexedDBが使えません");
 }
+
+var html5rocks = {};
+html5rocks.indexedDB = {};
+html5rocks.indexedDB.db = null;
+html5rocks.indexedDB.open = function() {
+  var version = 1;
+  var request = indexedDB.open("todos", version);
+
+  request.onsuccess = function(e) {
+    html5rocks.indexedDB.db = e.target.result;
+    // Do some more stuff in a minute
+  };
+
+  request.onerror = html5rocks.indexedDB.onerror;
+};
